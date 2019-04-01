821a819398ca58cc317f0c1c284f7ba76ee03776
Are Ehnberg <are.ehnberg@ntig.se>
2019-04-01 15:38:36 +0200

    Imported HangmanConsoleWindow resource and tested creating a window in the main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import hangman.HangmanConsoleWindow;
 
 public class Main {
 
+	HangmanConsoleWindow window = new HangmanConsoleWindow();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
