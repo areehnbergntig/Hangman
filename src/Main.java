9c70e09b9d25ca36a2ad19b13132ce273bc61424
Are Ehnberg <are.ehnberg@ntig.se>
2019-04-01 15:53:42 +0200

    Created necessary static variables and commented them

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	static HangmanConsoleWindow window = new HangmanConsoleWindow();
 	
+	final static String[] allWords = {"dog", "cat", "bird"}; // A list of words that can be used to play the game.
 	
+	static String wordProgress; 	// A String which starts out as for example "---" and gets updated with letters as the player guesses correctly.
+	static String correctWord;		// The correct word that the player is supposed to guess. Is used to update the String wordProgress as the game progresses and is used for different comparisons.
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
