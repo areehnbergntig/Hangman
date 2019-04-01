8c8cc9c3e3d85324df7bc916469e68e48b3f7abf
Are Ehnberg <are.ehnberg@ntig.se>
2019-04-01 15:46:30 +0200

    Corrected window variable to static

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import hangman.HangmanConsoleWindow;
 
 public class Main {
 
-	HangmanConsoleWindow window = new HangmanConsoleWindow();
+	static HangmanConsoleWindow window = new HangmanConsoleWindow();
+	
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
