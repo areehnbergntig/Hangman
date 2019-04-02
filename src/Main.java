1d6b9dd79a99c785f322cd26034b367cd837c54a
Are Ehnberg <are.ehnberg@ntig.se>
2019-04-02 10:23:07 +0200

    Created all methods in main class from method planning document

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,101 @@ public class Main {
 	static String correctWord;		// The correct word that the player is supposed to guess. Is used to update the String wordProgress as the game progresses and is used for different comparisons.
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		
 		
 	}
+	
+	
+	
+	/* -------------------------- */
+	/* --- Big game methods ----- */
+	/* -------------------------- */
+	
+	public static boolean oneGame() {
+		
+		
+		return false; //placeholder
+	}
+
+	public static boolean oneRound() {
+		
+		
+		return false; //placeholder
+	}
+
+	public static boolean oneGuess() {
+		
+		
+		return false; //placeholder
+	}
+	
+	
+
+	/* -------------------------- */
+	/* --- Small game methods --- */
+	/* -------------------------- */
+	
+	public static String randomizeWord() {
+		
+		
+		return ""; //placeholder
+	}
+
+	public static void goodGuess() {
+		
+	}
+
+	public static void badGuess() {
+		
+	}
+	
+	
+
+	/* -------------------------- */
+	/* --- Comparison methods --- */
+	/* -------------------------- */
+
+	public static boolean hasGuessedCorrectly() {
+		
+		
+		return false; //placeholder
+	}
+
+	public static boolean hasWon() {
+		
+		
+		return false; //placeholder
+	}
+
+	public static boolean wantToPlayAgain() {
+		
+		
+		return false; //placeholder
+	}
+	
+	
+
+	/* -------------------------- */
+	/* --- Message methods ------ */
+	/* -------------------------- */
+	
+	public static void printStartOfGameMessage() {
+		
+	}
+
+	public static void printStartOfRoundMessage() {
+		
+	}
+
+	public static void printWinningMessage() {
+		
+	}
+	
+	public static void printEndOfSessionMessage() {
+		
+	}
+	
+	
 
 }
